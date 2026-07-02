414f75736323e4fdb69ed173543f5263a14a74dc
CarmineSapo <carminesapo@gmail.com>
2026-07-02 20:02:16 +0200

    added authentication filter for page protection

---
+++ src/main/java/filter/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter( urlPatterns = {
+        "/profile",
+        "/my-orders",
+        "/checkout"
+})
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+
+        boolean authenticated = session != null && session.getAttribute("user") != null;
+
+        if (authenticated){
+            filterChain.doFilter(request, response);
+        }
+        else{
+            response.sendRedirect( request.getContextPath() + "/login.jsp");
+        }
+
+    }
+}
